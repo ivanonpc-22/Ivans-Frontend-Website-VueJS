5d5e38e940e394828667a3cbe2a3409da40f4ca4
Ivan <owner@ivanservers.com>
2022-05-05 15:34:48 +0900

    mobile compatability - V1.02

--- src/components/porfoliohomedesktop641227.vue
+++ src/components/porfoliohomedesktop641227.vue
@@ -20,12 +20,12 @@
     <span class="porfoliohomedesktop641227-text05">
       <span class="porfoliohomedesktop641227-text06">const</span>
       <span class="porfoliohomedesktop641227-text07">
-        <span v-html="rawid12"></span>
+        <span v-html="rawmn98"></span>
       </span>
       <span class="porfoliohomedesktop641227-text08">githubLink</span>
       <span class="porfoliohomedesktop641227-text09">
         =
-        <span v-html="raw96yy"></span>
+        <span v-html="raw79oi"></span>
       </span>
       <span class="porfoliohomedesktop641227-text10">“</span>
       <span class="porfoliohomedesktop641227-text11">
@@ -210,8 +210,8 @@ export default {
 
   data() {
     return {
-      rawid12: ' ',
-      raw96yy: ' ',
+      rawmn98: ' ',
+      raw79oi: ' ',
     }
   },
 }
